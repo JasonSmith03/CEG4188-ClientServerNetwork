6278bdea3c584cd1d5e6b393b1f62f44d0ab407d
Bradley Rose <43153077+btrose98@users.noreply.github.com>
2020-10-02 18:01:41 -0400

    Update client.py

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host = socket.gethostname() #get local machine name
 port = 8080
 
-client_socket.connect((host, port)) #NOTE: this isn't connecting to the server, you need to connect to the IP address of the server rather than your local machine
+client_socket.connect((host, port))
 #client_socket.send("Hello World \n")
 
 from_server = client_socket.recv(4096)
