7544a28567a0a2860bfcef99bc4492a762c81016
Jason <jasonasmith03@gmail.com>
2020-10-05 18:51:59 -0400

    Working application

--- client.py
+++ client.py
@@ -3,68 +3,41 @@
 #Client application for chat messaging
 
 import socket
-import select
-import errno
-from pip._vendor.distlib.compat import raw_input
-import sys
+import threading
 
-# serverName = "servername"
-# serverPort = 12000
-# clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# clientSocket.connect(("127.0.0.1",serverPort))
-# sentence = raw_input("Input lowercase sentence:")
-# clientSocket.send(sentence.encode())
-# modifiedSentence = clientSocket.recv(1024)
-# print ("From Server:", modifiedSentence.decode())
-# clientSocket.close()
+# Choosing Nickname
+nickname = input("Choose your nickname: ")
 
-HEADER_LENGTH = 10
-ip = socket.gethostname()
-port = 8080
+# Connecting To Server
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 55555))
 
-my_username = input("Username: ")
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((ip, port))
-client_socket.setblocking(False)
+# Listening to Server and Sending Nickname
+def receive():
+    while True:
+        try:
+            # Receive Message From Server
+            # If 'NICK' Send Nickname
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICK':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+            # Close Connection When Error
+            print("An error occured!")
+            client.close()
+            break
 
-username = my_username.encode('utf-8')
-username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
-client_socket.send(username_header + username)
+# Sending Messages To Server
+def write():
+    while True:
+        message = '{}: {}'.format(nickname, input(''))
+        client.send(message.encode('ascii'))
 
-while True:
-    message = input(f"{my_username} > ")
+# Starting Threads For Listening And Writing
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
 
-    if message:
-        message = message.encode('utf-8')
-        message_header = f"{len(message):< {HEADER_LENGTH}}".encode('utf-8')
-        client_socket.send(message_header + message)
-
-    try:
-        while True:
-            # receive messages
-            username_header = client_socket.recv(HEADER_LENGTH)
-            if not len(username_header):
-                print("Connection closed by the server")
-                sys.exit()
-            username_length = int(username_header.decode('utf-8').strip())
-            username = client_socket.recv(username_length).decode('utf-8')
-
-            message_header = client_socket.recv(HEADER_LENGTH)
-            message_length = int(message_header.decode('utf-8').strip())
-            message = client_socket.recv(message_length).decode('utf-8')
-
-            print(f"{username} > {message}")
-
-    except IOError as e:
-        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-            print("Reading error", str(e))
-            sys.exit()
-        continue
-
-    except Exception as e:
-        print("General error", str(e))
-        sys.exit()
-
-
-
-
+write_thread = threading.Thread(target=write)
+write_thread.start()
--- server.py
+++ server.py
@@ -3,75 +3,63 @@
 #Server application for chat messaging
 
 import socket
-import select
+import threading
 
-def receive_message(client_socket):
-    try:
-        message_header = client_socket.recv(HEADER_LENGTH)
-        if not len(message_header):
-            return False
-        message_length = int(message_header.decode("utf-8").strip())
-        return {"header": message_header, "data": client_socket.recv(message_length)}
-    except:
-        return False
+# Connection Data
+host = '127.0.0.1'
+port = 55555
 
+# Starting Server
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host, port))
+server.listen()
 
-HEADER_LENGTH = 10
-ip = socket.gethostname() #get local machine name
-port = 8080
+# Lists For Clients and Their Nicknames
+clients = []
+nicknames = []
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind((ip, port))
-server_socket.listen(5)
+# Sending Messages To All Connected Clients
+def broadcast(message):
+    for client in clients:
+        client.send(message)
 
-sockets_list = [server_socket]
-clients = {}
+# Handling Messages From Clients
+def handle(client):
+    while True:
+        try:
+            # Broadcasting Messages
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            # Removing And Closing Clients
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast('{} left!'.format(nickname).encode('ascii'))
+            nicknames.remove(nickname)
+            break
 
-while True:
-    read_sockets, _, exception_socket = select.select(sockets_list, [], sockets_list)
+# Receiving / Listening Function
+def receive():
+    while True:
+        # Accept Connection
+        client, address = server.accept()
+        print("Connected with {}".format(str(address)))
 
-    for notified_socket in read_sockets:
-        if notified_socket == server_socket:
-            client_socket, client_address = server_socket.accept()
-            user = receive_message(client_socket)
-            if user is False:
-                continue
-                sockets_list.append(client_socket)
-                clients[client_socket] = user
-                print(f"Accepted new connection from {client_address[0]}:{client_address[1]} username:{user['data'].decode('utf-8')}")
-            else:
-                message = receive_message(notified_socket)
-                if message is False:
-                    print(f"Closed Connection from {clients[notified_socket]['data'].decode('utf-8')}")
-                    sockets_list.remove(notified_socket)
-                    del clients[notified_socket]
-                    continue
-                user = clients[notified_socket]
-                print(f"Received message from {user['data'].decode('utf-8')}: {message['data'].decode('utf-8')}")
+        # Request And Store Nickname
+        client.send('NICK'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
 
-                for client_socket in clients:
-                    if client_socket != notified_socket:
-                        client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+        # Print And Broadcast Nickname
+        print("Nickname is {}".format(nickname))
+        broadcast("{} joined!".format(nickname).encode('ascii'))
+        client.send('Connected to server!'.encode('ascii'))
 
-    for notified_socket in exception_socket:
-        sockets_list.remove(notified_socket)
-        del clients[notified_socket]
+        # Start Handling Thread For Client
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
 
-
-
-# while True:
-#     conn, addr = server_socket.accept()
-#     from_client = ""
-#
-#     while True:
-#         data = conn.recv(4096)
-#         if not data: break
-#         from_client += data
-#         print(from_client)
-#
-#         conn.send("I am SERVER \n")
-#     conn.close()
-#     print("client disconnected")
-
-
+receive()
